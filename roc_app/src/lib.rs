0ac215b393ee4f7d6b46323a272203e7dc9fb0bb
Folkert <folkert@folkertdev.nl>
2023-08-09 17:01:28 +0200

    for the app, roc_alloc will return a NonNull<u8>

--- roc_app/src/lib.rs
+++ roc_app/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ptr::NonNull;
+
 struct AppAlloc;
 
 unsafe impl Sync for AppAlloc {}
@@ -8,10 +10,10 @@ static ALLOCATOR: AppAlloc = AppAlloc;
 unsafe impl std::alloc::GlobalAlloc for AppAlloc {
     unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
         extern "C" {
-            fn roc_alloc(size: usize, alignment: u32) -> *mut u8;
+            fn roc_alloc(size: usize, alignment: u32) -> NonNull<u8>;
         }
 
-        roc_alloc(layout.size(), layout.align() as u32)
+        roc_alloc(layout.size(), layout.align() as u32).as_ptr()
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: core::alloc::Layout) {
