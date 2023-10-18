46c69ad5686284fa201e4090dcd77e8b8f67b552
Folkert <folkert@folkertdev.nl>
2023-10-18 10:23:34 +0200

    bugz bugz bugz

---
+++ async-server/src/main.rs
@@ -0,0 +1,311 @@
+use std::{
+    cell::UnsafeCell,
+    io::{Cursor, ErrorKind},
+    net::{TcpListener, TcpStream},
+    os::fd::AsRawFd,
+    ptr::NonNull,
+    sync::atomic::{AtomicU32, AtomicUsize, Ordering},
+};
+
+mod config;
+mod executor;
+mod index;
+mod reactor;
+
+use config::Config;
+use executor::{BucketIndex, Executor};
+use index::QueueIndex;
+use reactor::Reactor;
+use shared::setjmp_longjmp::{longjmp, setjmp, JumpBuf};
+
+thread_local! {
+    /// The buffer used to store the state of registers before calling application code.core
+    ///
+    /// An error in the application code (out of memory, a panic, a segfault) will restore the
+    /// non-volatile registers to their previous state, so that a thread can recover from an error
+    /// in the application.
+    static JMP_BUFFER: UnsafeCell<JumpBuf> = UnsafeCell::new(JumpBuf::new());
+
+    static CURRENT_BUCKET: AtomicUsize = AtomicUsize::new(usize::MAX);
+}
+
+const ARENA_INDEX_BEFORE_MAIN: u32 = u32::MAX;
+pub(crate) const ARENA_INDEX_EXECUTOR: u32 = u32::MAX - 1;
+pub(crate) const ARENA_INDEX_REACTOR: u32 = u32::MAX - 2;
+const ARENA_INDEX_UNINITIALIZED: u32 = u32::MAX - 3;
+
+// pub(crate) static IS_RUNNING: AtomicBool = AtomicBool::new(false);
+
+thread_local! {
+    /// Which arena the current thread allocates into. By default this is bucket "infinity", a
+    /// statically allocated buffer meant for allocations before `main` and for initializing the
+    /// worker threads. After the worker threads have been spawned, nothing should be allocated in
+    /// the "infinity" bucket.
+    ///
+    /// The value is an atomic because that makes updating the value easier. This is a
+    /// thread-local, so race conditions are not possible!
+    pub(crate) static CURRENT_ARENA: AtomicU32 = AtomicU32::new(ARENA_INDEX_BEFORE_MAIN);
+}
+
+extern "C-unwind" {
+    // The application main function.
+    //
+    // Because this is a rust app, we use the `C-unwind` calling convention. That makes it possible
+    // for the app to panic, and for the server to catch that panic and recover. `C-unwind` will
+    // not be useful with roc applications, but it will still be correct.
+    #[allow(improper_ctypes)]
+    fn roc_main(input: String) -> String;
+}
+
+/// Function called when the applications hits a (from its perspective) unrecoverable error.
+///
+/// For instance: out of memory, an assert, division by zero
+pub unsafe extern "C" fn roc_panic(message_ptr: *const i8, panic_tag: u32) -> ! {
+    let thread_id = std::thread::current().id();
+
+    assert!(!message_ptr.is_null());
+
+    let message_cstr = unsafe { std::ffi::CStr::from_ptr(message_ptr) };
+    let message = message_cstr.to_str().unwrap();
+
+    eprintln!("thread {thread_id:?} hit a panic {panic_tag}: {message}");
+
+    JMP_BUFFER.with(|env| unsafe { longjmp(env.get(), 1) })
+}
+
+/// Core primitive for the application's allocator.
+pub unsafe extern "C" fn roc_alloc(size: usize, alignment: u32) -> NonNull<u8> {
+    let bucket_index = CURRENT_ARENA.with(|v| v.load(Ordering::Relaxed)) as usize;
+    assert!(bucket_index < 1000);
+
+    let layout = std::alloc::Layout::from_size_align(size, alignment as usize).unwrap();
+
+    let size = layout.size();
+
+    eprintln!("arena {bucket_index}: allocating {size} bytes",);
+
+    match ALLOCATOR.0.try_allocate_in_bucket(layout, bucket_index) {
+        None => {
+            let msg = b"out of memory\0";
+            let panic_tag = 1;
+            roc_panic(msg.map(|x| x as std::ffi::c_char).as_ptr(), panic_tag)
+        }
+        Some(non_null) => non_null,
+    }
+}
+
+/// Global Allocator for the Server
+///
+/// This is where the magic happens
+struct ServerAlloc(shared::allocator::ServerAlloc);
+
+#[global_allocator]
+static ALLOCATOR: ServerAlloc = ServerAlloc(shared::allocator::ServerAlloc::new());
+
+unsafe impl std::alloc::GlobalAlloc for ServerAlloc {
+    unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        let _size = layout.size();
+
+        match CURRENT_ARENA.with(|v| v.load(Ordering::Relaxed)) {
+            self::ARENA_INDEX_BEFORE_MAIN => {
+                log::info!("bucket ∞: allocating {_size} bytes",);
+
+                match self.0.try_allocate_in_initial_bucket(layout) {
+                    None => std::ptr::null_mut(), // a panic would be UB!
+                    Some(non_null) => non_null.as_ptr(),
+                }
+            }
+            index @ (self::ARENA_INDEX_EXECUTOR | self::ARENA_INDEX_REACTOR) => {
+                let bucket_name = match index {
+                    self::ARENA_INDEX_EXECUTOR => "executor",
+                    self::ARENA_INDEX_REACTOR => "reactor",
+                    _ => unreachable!(),
+                };
+
+                let id = std::thread::current().id();
+
+                // NOTE: this piece of code runs before main, so before the logger is setup
+                // hence we cannot use `log::*` here!
+                log::info!("thread {id:?}: bucket {bucket_name}: allocating {_size} bytes",);
+
+                match self.0.try_allocate_in_initial_bucket(layout) {
+                    None => std::ptr::null_mut(), // a panic would be UB!
+                    Some(non_null) => non_null.as_ptr(),
+                }
+            }
+            self::ARENA_INDEX_UNINITIALIZED => {
+                let thread = std::thread::current().id();
+                eprintln!("{thread:?}: about to allocate in uninitialized bucket");
+                std::ptr::null_mut()
+            }
+            bucket_index => {
+                log::trace!("bucket {bucket_index}: allocating {_size} bytes",);
+
+                let msg = b"out of memory\0";
+                let panic_tag = 1;
+
+                match self.0.try_allocate_in_bucket(layout, bucket_index as usize) {
+                    None => roc_panic(msg.map(|x| x as std::ffi::c_char).as_ptr(), panic_tag),
+                    Some(non_null) => non_null.as_ptr(),
+                }
+            }
+        }
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: std::alloc::Layout) {
+        /* do nothing */
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    log::init();
+
+    let config = Config::load();
+
+    ALLOCATOR.0.initialize_buckets(config.bucket_count, 4096)?;
+
+    let executor = Executor::get_or_init(config.bucket_count, config.io_resources);
+    let reactor = Reactor::get_or_init(config.bucket_count, config.io_resources).unwrap();
+
+    let _handle1 = executor.spawn_worker().unwrap();
+    let _handle2 = executor.spawn_worker().unwrap();
+
+    let addr = format!("{}:{}", config.host, config.port);
+    let listener = std::net::TcpListener::bind(&addr)?;
+    let id = std::thread::current().id();
+    println!("listening on http://{addr} on thread {id:?}");
+
+    // crate::IS_RUNNING.store(true, std::sync::atomic::Ordering::Relaxed);
+
+    // accept connections and process them serially
+    for (i, stream) in listener.incoming().enumerate() {
+        let stream = stream.unwrap();
+        stream.set_nonblocking(true).unwrap();
+        let fd = stream.as_raw_fd();
+
+        match executor.try_claim() {
+            None => {
+                // no space in the queue
+                log::warn!("main: no space in the queue");
+                stream.shutdown(std::net::Shutdown::Both).unwrap();
+            }
+            Some(bucket_index) => {
+                executor.execute(bucket_index, async move {
+                    log::info!(
+                        "new connection {i} (index = {}, fd = {fd})",
+                        bucket_index.index
+                    );
+
+                    let queue_index =
+                        QueueIndex::from_bucket_index(config.io_resources, bucket_index);
+                    let tcp_stream = reactor.register(queue_index, stream).unwrap();
+
+                    match handler(bucket_index, tcp_stream).await {
+                        Ok(()) => {}
+                        Err(e) => match e.kind() {
+                            ErrorKind::NotConnected => {}
+                            _ => Err(e).unwrap(),
+                        },
+                    }
+                });
+            }
+        };
+
+        log::info!("main spawned future for connection {i}");
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Cmd {
+    Read,
+    Write(Vec<u8>),
+    Done,
+}
+
+#[derive(Debug)]
+enum Msg {
+    Nothing,
+    Read(Vec<u8>),
+}
+
+#[derive(Debug)]
+enum Model {
+    Initial,
+    Reading(Vec<u8>),
+    Writing,
+}
+
+fn roc_handler(model: Model, msg: Msg) -> (Model, Cmd) {
+    match (model, msg) {
+        (Model::Initial, Msg::Nothing) => {
+            // let's read something
+            (Model::Initial, Cmd::Read)
+        }
+        (Model::Initial, Msg::Read(buf)) => {
+            //
+            drop(buf);
+
+            use std::io::Write;
+            let input = "foobar";
+            let mut buffer = [0u8; 1024];
+            let mut response = Cursor::new(&mut buffer[..]);
+            let _ = response.write_all(b"HTTP/1.1 200 OK\r\n");
+            let _ = response.write_all(b"Content-Type: text/html\r\n");
+            let _ = response.write_all(b"\r\n");
+            let _ = response.write_fmt(format_args!("<html>{input}</html>"));
+            let n = response.position() as usize;
+
+            let buf = response.get_ref()[..n].to_vec();
+
+            (Model::Writing, Cmd::Write(buf))
+        }
+        (Model::Reading(mut buf), Msg::Read(new)) => {
+            // assume eof
+            buf.extend(new);
+            (Model::Writing, Cmd::Write(buf))
+            //            } else {
+            //                // continue reading
+            //                buf.extend(new);
+            //                (Model::Reading(buf), Cmd::Read)
+            //            }
+        }
+        (Model::Reading(_), Msg::Nothing) => unreachable!(),
+        (Model::Writing, _) => (Model::Writing, Cmd::Done),
+    }
+}
+
+async fn handler(
+    _bucket_index: BucketIndex,
+    tcp_stream: crate::reactor::TcpStream,
+) -> std::io::Result<()> {
+    let mut msg = Msg::Nothing;
+    let mut model = Model::Initial;
+
+    loop {
+        let (new_model, cmd) = roc_handler(model, msg);
+        model = new_model;
+
+        match cmd {
+            Cmd::Read => {
+                let mut buf = vec![0u8; 1024];
+                let n = tcp_stream.read(&mut buf).await?;
+
+                buf.truncate(n);
+                msg = Msg::Read(buf);
+            }
+            Cmd::Write(buf) => {
+                let mut remaining = buf.len();
+                while remaining > 0 {
+                    remaining -= tcp_stream.write(&buf).await?;
+                }
+
+                msg = Msg::Nothing;
+            }
+            Cmd::Done => break,
+        }
+    }
+
+    Ok(())
+}
---
+++ async-server/src/reactor.rs
@@ -0,0 +1,363 @@
+// https://github.com/ibraheemdev/astra/blob/53ad0859de7a1e2af90d8ae1a6666c9a7a276c03/src/net.rs#L13
+
+use hyper::rt::ReadBufCursor;
+use std::io::Error;
+use std::mem::MaybeUninit;
+use std::pin::Pin;
+use std::{
+    io::Write,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Mutex, OnceLock,
+    },
+    task::{Context, Poll, Waker},
+};
+
+use mio::{Events, Interest, Token};
+
+use crate::index::{IoResources, QueueIndex};
+use crate::CURRENT_ARENA;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Read = 0,
+    Write = 1,
+}
+
+struct Source {
+    interest: Mutex<[Option<Waker>; 2]>,
+    triggered: [AtomicBool; 2],
+}
+
+#[derive(Clone, Copy)]
+pub struct Reactor {
+    shared: &'static Shared,
+}
+
+static SHARED: OnceLock<Shared> = OnceLock::new();
+
+impl Reactor {
+    pub fn get() -> Option<Self> {
+        SHARED.get().map(|shared| Reactor { shared })
+    }
+
+    pub fn get_or_init(bucket_count: usize, io_resources: IoResources) -> std::io::Result<Self> {
+        match SHARED.get() {
+            None => {
+                let queue_capacity = bucket_count * io_resources.per_bucket();
+                let poll = mio::Poll::new()?;
+
+                let shared = Shared {
+                    registry: poll.registry().try_clone()?,
+                    sources: std::iter::repeat_with(|| Mutex::new(None))
+                        .take(queue_capacity)
+                        .collect(),
+                    io_resources,
+                };
+
+                let shared = SHARED.get_or_init(|| shared);
+
+                std::thread::Builder::new()
+                    .name(String::from("nea-reactor"))
+                    .spawn(move || shared.run(poll))?;
+
+                Ok(Reactor { shared })
+            }
+            Some(shared) => {
+                // nothing to do
+                Ok(Reactor { shared })
+            }
+        }
+    }
+
+    pub fn register(
+        &self,
+        index: QueueIndex,
+        tcp_stream: std::net::TcpStream,
+    ) -> std::io::Result<TcpStream> {
+        tcp_stream.set_nonblocking(true).unwrap();
+
+        let mut tcp_stream = mio::net::TcpStream::from_std(tcp_stream);
+        let token = Token(index.to_usize());
+
+        let source = Source {
+            interest: Default::default(),
+            triggered: Default::default(),
+        };
+
+        let old = self.shared.sources[index.index as usize]
+            .lock()
+            .unwrap()
+            .replace(source);
+        debug_assert!(old.is_none());
+
+        // IMPORTANT: only register when everything is in place to handle events on this fd
+        self.shared
+            .registry
+            .register(
+                &mut tcp_stream,
+                token,
+                Interest::READABLE | Interest::WRITABLE,
+            )
+            .unwrap();
+
+        let tcp_stream = TcpStream {
+            tcp_stream,
+            reactor: *self,
+            token,
+        };
+
+        Ok(tcp_stream)
+    }
+
+    fn poll_ready(
+        &self,
+        source: &Source,
+        direction: Direction,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        if source.triggered[direction as usize].load(Ordering::Acquire) {
+            return Poll::Ready(Ok(()));
+        }
+
+        // update the waker if required
+        {
+            let mut interest = source.interest.lock().expect("poll_ready to take the lock");
+
+            match &mut interest[direction as usize] {
+                Some(existing) if existing.will_wake(cx.waker()) => {
+                    /* has the right waker already */
+                }
+                other => {
+                    *other = Some(cx.waker().clone());
+                }
+            }
+        }
+
+        // check if anything changed while we were registering our waker
+        if source.triggered[direction as usize].load(Ordering::Acquire) {
+            // just wake up immediately
+            return Poll::Ready(Ok(()));
+        }
+
+        Poll::Pending
+    }
+
+    fn clear_trigger(&self, source: &Source, direction: Direction) {
+        source.triggered[direction as usize].store(false, Ordering::Release);
+    }
+}
+
+struct Shared {
+    registry: mio::Registry,
+    sources: Vec<Mutex<Option<Source>>>,
+    io_resources: IoResources,
+}
+
+unsafe impl Sync for Shared {}
+
+impl Shared {
+    fn run(&self, mut poll: mio::Poll) -> std::io::Result<()> {
+        CURRENT_ARENA.with(|a| a.store(crate::ARENA_INDEX_REACTOR, Ordering::Relaxed));
+
+        let queue_capacity = self.sources.len() * self.io_resources.per_bucket();
+        let mut events = Events::with_capacity(queue_capacity);
+        let mut wakers = Vec::with_capacity(queue_capacity);
+
+        loop {
+            if let Err(err) = self.poll(&mut poll, &mut events, &mut wakers) {
+                log::warn!("Failed to poll reactor: {}", err);
+            }
+
+            events.clear()
+        }
+    }
+
+    fn poll(
+        &self,
+        poll: &mut mio::Poll,
+        events: &mut Events,
+        wakers: &mut Vec<Waker>,
+    ) -> std::io::Result<()> {
+        // TODO figure out why this is specifically?
+        if let Err(err) = poll.poll(events, None) {
+            if err.kind() != std::io::ErrorKind::Interrupted {
+                return Err(err);
+            }
+
+            log::warn!("mio poll hit an error {:?}", err);
+
+            return Ok(());
+        }
+
+        for event in events.iter() {
+            let task_index = QueueIndex::from_usize(event.token().0);
+            let index = task_index.index as usize;
+            let source = self.sources[index].lock().unwrap();
+
+            let source = match source.as_ref() {
+                None => continue,
+                Some(source) => source,
+            };
+
+            let mut interest = source.interest.lock().expect("event loop");
+
+            if event.is_readable() {
+                // TODO when is this waker None?
+                if let Some(waker) = interest[Direction::Read as usize].take() {
+                    wakers.push(waker);
+                }
+
+                // TODO why release?
+                source.triggered[Direction::Read as usize].store(true, Ordering::Release);
+            }
+
+            if event.is_writable() {
+                // TODO when is this waker None?
+                if let Some(waker) = interest[Direction::Write as usize].take() {
+                    wakers.push(waker);
+                }
+
+                // TODO why release?
+                source.triggered[Direction::Write as usize].store(true, Ordering::Release);
+            }
+        }
+
+        for waker in wakers.drain(..) {
+            waker.wake();
+        }
+
+        Ok(())
+    }
+}
+
+pub struct TcpStream {
+    tcp_stream: mio::net::TcpStream,
+    reactor: Reactor,
+    token: Token,
+}
+
+impl TcpStream {
+    pub fn poll_io<T>(
+        &self,
+        direction: Direction,
+        mut f: impl FnMut() -> std::io::Result<T>,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<T>> {
+        loop {
+            let index = QueueIndex::from_usize(self.token.0);
+
+            let source_guard = self.reactor.shared.sources[index.index as usize]
+                .lock()
+                .unwrap();
+
+            let Some(source) = source_guard.as_ref() else {
+                log::warn!("this source got terminated");
+                return Poll::Pending;
+            };
+
+            std::task::ready!(self.reactor.poll_ready(source, direction, cx))?;
+
+            match f() {
+                Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
+                    self.reactor.clear_trigger(source, direction);
+                }
+                val => {
+                    return Poll::Ready(val);
+                }
+            }
+        }
+    }
+
+    pub async fn read(&self, buf: &mut [u8]) -> std::io::Result<usize> {
+        use std::io::Read;
+
+        std::future::poll_fn(|cx| {
+            self.poll_io(Direction::Read, || (&self.tcp_stream).read(buf), cx)
+        })
+        .await
+    }
+
+    pub async fn write(&self, buf: &[u8]) -> std::io::Result<usize> {
+        std::future::poll_fn(|cx| {
+            self.poll_io(Direction::Write, || (&self.tcp_stream).write(buf), cx)
+        })
+        .await
+    }
+}
+
+impl hyper::rt::Read for TcpStream {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        mut buf: ReadBufCursor<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        const TMP_BUF_LEN: usize = 1024;
+        use std::io::Read;
+
+        let buf_mut = unsafe { buf.as_mut() };
+        let mut tmp_buf = [0; TMP_BUF_LEN];
+        let remaining = buf_mut.len().min(TMP_BUF_LEN);
+
+        let poll = self.poll_io(
+            Direction::Read,
+            || (&self.tcp_stream).read(&mut tmp_buf[..remaining]),
+            cx,
+        );
+
+        let n = std::task::ready!(poll)?;
+        let tmp_buf = tmp_buf.map(MaybeUninit::new);
+
+        buf_mut[..n].copy_from_slice(&tmp_buf[..n]);
+
+        unsafe {
+            buf.advance(n);
+        }
+
+        Poll::Ready(Ok(()))
+    }
+}
+
+impl hyper::rt::Write for TcpStream {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {
+        let n = std::task::ready!(self.poll_io(
+            Direction::Write,
+            || (&self.tcp_stream).write(buf),
+            cx
+        ))?;
+
+        Poll::Ready(Ok(n))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        self.poll_io(Direction::Write, || (&self.tcp_stream).flush(), cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        self.poll_io(
+            Direction::Write,
+            || self.tcp_stream.shutdown(std::net::Shutdown::Both),
+            cx,
+        )
+    }
+}
+
+impl Drop for TcpStream {
+    fn drop(&mut self) {
+        let index = QueueIndex::from_usize(self.token.0).index as usize;
+        log::info!("token {} removed from poll", index);
+
+        let _ = self
+            .reactor
+            .shared
+            .registry
+            .deregister(&mut self.tcp_stream);
+
+        let old = self.reactor.shared.sources[index].lock().unwrap().take();
+        assert!(old.is_some());
+    }
+}
