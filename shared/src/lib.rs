aefd864eda47443301fc65dc7f8daaf98508c964
Tamme Dittrich <tamme@tweedegolf.com>
2024-04-22 15:55:25 +0200

    Remove old unused code

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -2,18 +2,3 @@ pub mod allocator;
 pub mod indexer;
 pub mod queue;
 pub mod setjmp_longjmp;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
